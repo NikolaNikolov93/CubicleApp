29c009b6efbd3b8d4fa619367507e1967e0942fe
Nikola Nikolov <122821901+NikolaNikolov93@users.noreply.github.com>
2023-09-25 00:24:08 +0300

    CB1 - Implemeting configurations for handlebars and express

--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
-const { dir } = require("console");
 const express = require("express");
-const handlebars = require("express-handlebars");
-const path = require("path");
+const handlebarsConfig = require("./config/handlebarsConfig");
+const expressConfig = require("./config/expressConfig");
+const constants = require("./constants");
 
 const app = express();
-const PORT = 5050;
-//handlebars configurations
-app.engine("hbs", handlebars.engine({ extname: "hbs" }));
-app.set("view engine", "hbs");
-app.set("views", "src/views");
 
-//setup static files
-const staticFiles = express.static(path.resolve(__dirname, "public"));
-app.use(staticFiles);
+expressConfig(app);
+handlebarsConfig(app);
 
 app.get("/", (req, res) => {
   res.render("index");
 });
-app.listen(PORT, () => console.log(`Server is running on port: ${PORT}...`));
+app.listen(constants.PORT, () =>
+  console.log(`Server is running on port: ${constants.PORT}...`)
+);
