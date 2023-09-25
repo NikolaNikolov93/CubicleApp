5532e61c3c4186da5a2a21f1251cc45d2a6ee585
Nikola Nikolov <122821901+NikolaNikolov93@users.noreply.github.com>
2023-09-26 01:06:25 +0300

    updated routing and page render

--- src/index.js
+++ src/index.js
@@ -2,15 +2,14 @@ const express = require("express");
 const handlebarsConfig = require("./config/handlebarsConfig");
 const expressConfig = require("./config/expressConfig");
 const constants = require("./constants");
+const routes = require("./router");
 
 const app = express();
 
 expressConfig(app);
 handlebarsConfig(app);
-
-app.get("/", (req, res) => {
-  res.render("index");
-});
+app.use(routes);
+// app.use()
 app.listen(constants.PORT, () =>
   console.log(`Server is running on port: ${constants.PORT}...`)
 );
