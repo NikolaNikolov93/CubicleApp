55910400da16d7e5365869e8f9370ae14d8afc43
Nikola Nikolov <122821901+NikolaNikolov93@users.noreply.github.com>
2023-10-04 00:50:13 +0300

    CB-1 provided services to attach to cube

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -31,4 +31,10 @@ router.get("/:cubeId/attach-accessory", async (req, res) => {
   const hasAccessories = accessories.length > 0;
   res.render("accessory/attach", { cube, accessories, hasAccessories });
 });
+router.post("/:cubeId/attach-accessory", async (req, res) => {
+  const { cubeId } = req.params;
+  const { accessory: accessoryId } = req.body;
+  await cubeServices.attachAccessory(cubeId, accessoryId);
+  res.redirect(`/cubes/${cubeId}/details`);
+});
 module.exports = router;
