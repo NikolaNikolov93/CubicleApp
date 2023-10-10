bffdc37fb34bf1d440a1559cb03fc65cc7e031cf
Nikola Nikolov <122821901+NikolaNikolov93@users.noreply.github.com>
2023-10-10 23:46:45 +0300

    CB1 - applied cookies and jtw tokens

--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -1,7 +1,9 @@
 const path = require("path");
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const expressConfig = (app) => {
   app.use(express.static(path.resolve(__dirname, "../public")));
   app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
 };
 module.exports = expressConfig;
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,7 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
-
+const jwt = require("../lib/jwt");
+const { SECRET } = require("../constants");
 exports.register = (userData) => {
   return User.create(userData);
 };
@@ -13,5 +14,10 @@ exports.login = async (username, pasword) => {
   if (!isValid) {
     throw new Error("Invalid username or password");
   }
-  return user;
+  const payload = {
+    _id: user._id,
+    username: user.username,
+  };
+  const token = await jwt.sign(payload, SECRET, { expiresIn: "3d" });
+  return token;
 };
