5363cb8e0b38a28e315bf0a88e07b4bdd9844099
Nikola Nikolov <122821901+NikolaNikolov93@users.noreply.github.com>
2023-10-11 00:38:50 +0300

    CB1 - Auth middleware and attach owner when creating cube

--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -1,9 +1,11 @@
 const path = require("path");
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const { auth } = require("../middlewares/authMiddleware");
 const expressConfig = (app) => {
   app.use(express.static(path.resolve(__dirname, "../public")));
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
+  app.use(auth);
 };
 module.exports = expressConfig;
